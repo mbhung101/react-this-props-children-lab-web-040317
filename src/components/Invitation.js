95af21d22c6e4f889cb6d290b897f78237f4742e
github username <github email address>
2017-06-12 15:53:18 -0400

    has the right title content

--- src/components/Invitation.js
+++ src/components/Invitation.js
@@ -1,1 +1,21 @@
 // Code Invitation Component Here
+
+import React, {Component} from 'react'
+
+export default class Invitation extends Component {
+
+  constructor (){
+    super()
+  }
+
+  render(){
+    return (
+      <div>
+        <h1>You've been invited! </h1>
+          {this.props.children}
+      </div>
+    )
+  }
+
+
+}
--- src/components/ThemedDecorations.js
+++ src/components/ThemedDecorations.js
@@ -1,1 +1,25 @@
-// Code ThemedDecoration Component Here
+// Code ThemedDecoration Component Here\\
+
+import React, {Component} from 'react'
+
+export default class ThemedDecoration extends Component {
+
+  constructor (){
+    super()
+  }
+
+  render() {
+       const childrenWithWrapperDiv = React.Children.map(this.props.children, child => {
+         return (
+           <div className={this.props.theme}>{child}</div>
+         );
+       });
+       return(
+           <div>
+               {childrenWithWrapperDiv}
+           </div>
+       )
+   }
+
+
+}
